d2a9892cc32925acaa8e7f5e73ee5e2a315d9737
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-09-06 10:58:43 -0400

    Java review-06/09/2017

--- src/javareview.java
+++ src/javareview.java
@@ -1,8 +1,10 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author simon7323
@@ -13,6 +15,58 @@ public class javareview {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // initialize variables
+        //type name [= value];
+        int age = 10;
+        double length = 25.73;
+
+        //math ops
+        // + addition
+        // - subtraction
+        // * multiplication
+        // / division
+        // % modulus (remainder after division)
+        age = age + 10;
+        length = length * 2;
+        //adds one to age
+        age++;
+        //minus one to age
+        age--;
+
+        //conditions
+        if (length > 100) {
+            System.out.println("Length is bigger than 100.");
+        } else if (length > 50 && length <= 100) {
+            System.out.println("Length is between 50 and 100.");
+        } else {
+            System.out.println("Length is 50 or lower.");
+        }
+
+        // while loops
+        // && -> AND
+        // || -> OR
+        int count = 0;
+        while (count < 10) {
+            //increase count and print
+            count++;
+            System.out.println(count);
+        }
+
+        //for loop
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i:" + i);
+        }
+
+        //arrays
+        String[] words = new String[5];
+        words[0] = "cat";
+
+        //input
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < words.length; i++) {
+            System.out.println("Enter a word.");
+            words[i] = in.nextLine();
+            
+        }
     }
 }
