82b491f0e80a514eb6b1cfb31831be29b4a4e1b3
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-09-07 10:11:43 -0400

    07/09/2017: initialize A1Q2

---
+++ src/A1Q2.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author simon7323
+ */
+public class A1Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner for user input
+        Scanner input = new Scanner(System.in);
+        //create list of numbers
+        int[] integers = new int[3];
+        //ask user to input 3 numbers
+        System.out.println("Enter 3 numbers: ");
+        //scan in numbers
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = input.nextInt();
+        }
+        //create accumulator 
+        //find product amongst the 3 numbers
+        int product = 1;
+        for (int i=0;i<integers.length;i++){
+        product = product*integers[i];    
+        }
+       System.out.println(product); 
+       //square the product
+        double square = Math.pow(product,2);
+        System.out.println(square);
+       //square root of product
+        double squareroot = Math.sqrt(square);
+        System.out.println(squareroot);
+        
+        
+    }
+}
