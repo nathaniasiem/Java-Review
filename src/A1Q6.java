5cb5cc572f99aa5fda4a714c2c52489d5d882f0c
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-09-08 10:14:00 -0400

    08/09/2017:A1Q6 initialize

---
+++ src/A1Q6.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author simon7323
+ */
+public class A1Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scanner
+        Scanner input = new Scanner (System.in);
+        //ask user to input number between 50-100
+        System.out.println("Enter the number between 50 and 100.");
+        int num =input.nextInt();
+        
+        
+            
+        }
+    }
+}
