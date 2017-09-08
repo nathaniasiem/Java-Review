bd58e77fa002163d30faa0a36ce020ee235891b2
simon7323 <simon7323@HRH-IC0027307.wrdsb.ca>
2017-09-08 11:15:19 -0400

    08/09/2017: A1Q6 3/4 completed

--- src/A1Q6.java
+++ src/A1Q6.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author simon7323
@@ -17,14 +16,23 @@ public class A1Q6 {
      */
     public static void main(String[] args) {
         // create a scanner
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
         //ask user to input number between 50-100
+
         System.out.println("Enter the number between 50 and 100.");
-        int num =input.nextInt();
-        //condition
-        //countsdown from 100
-        for(int i=100;i>=num;i=i-5){
-            System.out.println(i); 
+        int num = input.nextInt();
+        //condition for user to be unable to input numbers greater/lesser than 50 and 100
+        if (num > 100) {
+            System.out.println("ERROR.REENTER A NUMBER BETWEEN 50 AND 100");
+        } else if (num < 50) {
+            System.out.println("ERROR. REENTER A NUMBER BETWEEN 50 AND 100");
+
+            
+        } else {
+            //countsdown from 100
+            for (int i = 100; i >= num; i = i - 5) {
+                System.out.println(i);
+            }
         }
     }
 }
