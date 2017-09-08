3156ff04d704e3ced7abefc25062cbccf692713d
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-09-08 09:37:28 -0400

    08/09/2017:A1Q4 completed with correct format

---
+++ src/A1Q4.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author simon7323
+ */
+public class A1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner
+        Scanner input = new Scanner(System.in);
+        //ask user to enter a number from 1-10
+        System.out.println("Please enter a number between 1 and 10");
+        int num = input.nextInt();
+        
+        //create loop to count the number of asterisks user entered
+        for (int i = 0; i < num; i++) {
+            //print out the asterisks
+            System.out.print("*");
+        }
+    }
+}
+
