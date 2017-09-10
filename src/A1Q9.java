950d402bcc259f3bb05bb7cf3daa621fbfc6e53f
candy <candy@192.168.0.11>
2017-09-10 17:06:31 -0400

    10/09/2017-initial A1Q9

---
+++ src/A1Q9.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author candy
+ */
+public class A1Q9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scanner
+        Scanner input = new Scanner (System.in);
+        
+        //create an array to store in 10 words
+        String [] words = new String[10];
+        
+        //create variable to keep count of characters
+        int total = 0;
+    }
+    
+}
