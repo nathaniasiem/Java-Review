9c463b2acd31bbca58e8a8c5b989fe3f76551bb1
candy <candy@192.168.0.11>
2017-09-09 22:27:22 -0400

    07/09/2017-A1Q6 completed with formatting

--- src/A1Q6.java
+++ src/A1Q6.java
@@ -22,6 +22,7 @@ public class A1Q6 {
         while (true) {
             System.out.println("Enter the number between 50 and 100.");
             int num = input.nextInt();
+            String word = input.nextLine();
             //condition for user to be unable to input numbers greater/lesser than 50 and 100
 
             if (num <= 100 && num >= 50) {
@@ -31,6 +32,9 @@ public class A1Q6 {
                 }
             } else {
                 System.out.println("ERROR.REENTER A NUMBER BETWEEN 50 AND 100");
+            }
+            if (word.equals("END")) {
+                break;
             }
         }
     }
