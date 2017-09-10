9e0390c046e89fba97538a77597ffe6453e8074f
candy <candy@192.168.0.11>
2017-09-10 17:14:53 -0400

    10/09/2017-A1Q10 completed

--- src/A1Q10.java
+++ src/A1Q10.java
@@ -18,11 +18,19 @@ public class A1Q10 {
     public static void main(String[] args) {
         // create a scanner
         Scanner input = new Scanner(System.in);
-
+        
+        //create a variable to keep track number of words entered
+        int count = 0;
+        //change any word entered into lowercase
         //ask user to enter a word
         while (true) {
             System.out.println("Enter a word.");
             String word = input.nextLine();
+            count++;
+            if (word.equals("EXIT")){
+                System.out.println("You have entered "+(count-1)+" word(s).");
+                break;
+            }
         }
     }
 
