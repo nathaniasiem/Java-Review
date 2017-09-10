2ab03ba9456f07e7447d22ccb3c9faf2a6223ce5
candy <candy@192.168.0.11>
2017-09-10 01:36:54 -0400

    09/09/2017-A1Q7 completed with formatting

---
+++ src/A1Q7.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author candy
+ */
+public class A1Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner
+        Scanner input = new Scanner(System.in);
+        //set variable for starting year
+        int initYear = 1999;
+        //set variable for starting population
+        int initPop = 6;
+        //set variable for year
+        int year = 0;
+
+        //create formula to calculate rate
+        while (true) {
+            double futrPop = Math.pow(1.014, year);
+
+            double popRate = initPop * futrPop;
+            if (popRate > 10) {
+                System.out.println((year + initYear) + " will be the year that the population will exceed 10 billion ");
+                break;
+            }
+            year++;
+
+        }
+
+    }
+}
