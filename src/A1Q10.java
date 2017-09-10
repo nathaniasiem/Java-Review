f34da493fd10f61c8ccdda8c187d3d97e527084d
candy <candy@192.168.0.11>
2017-09-10 17:06:52 -0400

    10/09/2017-initial A1Q10

---
+++ src/A1Q10.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author candy
+ */
+public class A1Q10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scanner
+        Scanner input = new Scanner(System.in);
+
+        //ask user to enter a word
+        while (true) {
+            System.out.println("Enter a word.");
+            String word = input.nextLine();
+        }
+    }
+
+}
