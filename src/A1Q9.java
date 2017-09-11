650b461b9175c7675acf1e02612606008a8c24e1
simon7323 <simon7323@HRH-IC0027308.wrdsb.ca>
2017-09-11 10:54:13 -0400

    11/09/2017-A1Q9 completed with added comments

--- src/A1Q9.java
+++ src/A1Q9.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author candy
@@ -18,26 +17,30 @@ public class A1Q9 {
      */
     public static void main(String[] args) {
         // create a scanner
-        Scanner input = new Scanner (System.in);
-        
+        Scanner input = new Scanner(System.in);
+
         //create an array to store in 10 words
-        String [] words = new String[10];
-        
+        String[] words = new String[10];
+
         //create variable to keep count of characters
         int total = 0;
-        
-        //create loop to go through words
-        for(int i =0;i<10;i++){
+
+        //create loop to go through words within the array
+        //asks user ot enter a word
+        for (int i = 0; i < 10; i++) {
             System.out.println("Enter a word.");
-            words[i]=input.nextLine();
-                      
-        }for(int i=0;i<10;i++){
-            String term = words[i];
-            int count = term.length();
-            total =count+total;
+            words[i] = input.nextLine();
+            //create loop to keep track of number of letters              
         }
-        int avg = total/10;
-        
-        System.out.println("The average word length is "+avg+" character(s) long.");
+        for (int i = 0; i < 10; i++) {
+            String term = words[i];
+            //convert word to character length in numbers
+            int count = term.length();
+            //add to variable
+            total = count + total;
+        }
+        //calculate average word length
+        int avg = total / 10;
+        System.out.println("The average word length is " + avg + " character(s) long.");
     }
 }
