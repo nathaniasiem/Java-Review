c54747e2884375407059532cd1094ccc942e8e41
candy <candy@192.168.0.11>
2017-09-11 16:11:25 -0400

    11/09/2017-A1Q4 completed with comments and additional conditions

--- src/A1Q4.java
+++ src/A1Q4.java
@@ -17,20 +17,22 @@ public class A1Q4 {
     public static void main(String[] args) {
         // create scanner
         Scanner input = new Scanner(System.in);
-        //ask user to enter a number from 1-10
-        System.out.println("Please enter a number between 1 and 10");
-       
-        int num = input.nextInt();
-        if(num>10){
-            
-        }else if (num<1){
-            
-        }
-        //create loop to count the number of asterisks user entered
-        for (int i = 0; i < num; i++) {
-            //print out the asterisks
-            System.out.print("*");
+        //keep program running
+        while (true) {
+            //ask user to enter a number from 1-10
+            System.out.println("Please enter a number between 1 and 10");
+            int num = input.nextInt();
+            //condition if user entered a number not between 1 and 10
+            if (num > 10 || num < 1) {
+                System.out.println("error.");
+                //create loop to count the number of asterisks user entered
+            } else {
+                for (int i = 0; i < num; i++) {
+                    //print out the asterisks
+                    System.out.print("*");
+                }
+                break;
+            }
         }
     }
 }
-
