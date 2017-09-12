537ca154d03befd2a862270a528a20b0be215c38
candy <candy@192.168.0.11>
2017-09-11 22:04:58 -0400

    11/09/2017-A1Q6 modified comments and added new comments

--- src/A1Q6.java
+++ src/A1Q6.java
@@ -18,21 +18,22 @@ public class A1Q6 {
         // create a scanner
         Scanner input = new Scanner(System.in);
 
-        //ask user to input number between 50-100
+        //instruct user how to use program
         while (true) {
-            System.out.println("Enter the number between 50 and 100.");
+            System.out.println("Enter the number between 50 and 100.When finished type in END to end the program.");
             int num = input.nextInt();
             String word = input.nextLine();
-            //condition for user to be unable to input numbers greater/lesser than 50 and 100
-
+            //condition for user to input numbers between 50 and 100
             if (num <= 100 && num >= 50) {
                 //countsdown from 100
                 for (int i = 100; i >= num; i = i - 5) {
                     System.out.println(i);
                 }
+            // condition when number is greater than 100/less than 50
             } else {
                 System.out.println("ERROR.REENTER A NUMBER BETWEEN 50 AND 100");
             }
+            //condition when user wants to end program
             if (word.equals("END")) {
                 break;
             }
